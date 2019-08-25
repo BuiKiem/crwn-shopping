547b2f98044438ea75abf234cadcc3e6931cae00
Bui Kiem <buivantrung1105@gmail.com>
2019-08-25 16:08:41 +0700

    82 - Google Sign In Authentication 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom'
 
 import { Homepage } from './shop/pages/Homepage/Homepage';
 import { CollectionPage } from './shop/pages/CollectionPage/CollectionPage';
 import { SignInSignUpPage } from './shop/pages/SignInSignUpPage/SignInSignUpPage';
 import { Header } from './shop/components/Header/Header';
+import { auth } from './firebase/firebase.utils';
 
 import './App.css';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      console.log(user);
+    });
+
+  });
+
   return (
     <div>
       <Header />
