cf74fca61d254e8fca5e5f7d81cebf74205c0647
buikiem <buivantrung1105@gmail.com>
2019-08-27 16:34:14 +0700

    100 - connect() and mapStateToProps

--- src/shop/components/Header/Header.js
+++ src/shop/components/Header/Header.js
@@ -1,30 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 
 import './Header.scss';
 
-export const Header = ({ currentUser }) => (
-  <div className="header">
-    <Link to="/" className="logo-container">
-      <Logo className="logo" />
-    </Link>
-    <div className="options">
-      <Link to="/collection" className="option">SHOP</Link>
-      <Link to="/collection" className="option">CONTACT</Link>
-      {
-        currentUser
-          ? (
-            <div className="option" onClick={() => auth.signOut()}>
-              SIGN OUT
-            </div>
-          )
-          : (
-            <Link className="option" to="/signin">SIGN IN</Link>
-          )
-      }
+
+export const Header = () => {
+  const currentUser = useSelector((state) => state.user.currentUser);
+
+  return (
+    <div className="header">
+      <Link to="/" className="logo-container">
+        <Logo className="logo" />
+      </Link>
+      <div className="options">
+        <Link to="/collection" className="option">SHOP</Link>
+        <Link to="/collection" className="option">CONTACT</Link>
+        {
+          currentUser
+            ? (
+              <div className="option" onClick={() => auth.signOut()}>
+                SIGN OUT
+              </div>
+            )
+            : (
+              <Link className="option" to="/signin">SIGN IN</Link>
+            )
+        }
+      </div>
     </div>
-  </div>
-);
+  );
+};
