82f6108e7f3a0ee65f3bad693ecce0e28ddcde40
buikiem <buivantrung1105@gmail.com>
2019-08-29 12:51:26 +0700

    121 - Remove Item From Cart

--- src/shop/components/CheckoutItem/CheckoutItem.js
+++ src/shop/components/CheckoutItem/CheckoutItem.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import { clearItemFromCart } from '../../redux/cart/cartActions';
 
 import './CheckoutItem.scss';
 
 
-export const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img src={imageUrl} alt="item"/>
+export const CheckoutItem = ({ cartItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  const dispatch = useDispatch();
+  const clearItem = () => dispatch(clearItemFromCart(cartItem));
+
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item"/>
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">{quantity}</span>
+      <span className="price">{price}</span>
+      <span className="remove-button" onClick={clearItem}>&#10008;</span>
     </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <span className="remove-button">&#10008;</span>
-  </div>
-);
+  );
+};
