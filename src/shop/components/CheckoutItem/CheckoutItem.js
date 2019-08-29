5b17c9c6e430fadfcdf869f437cc194ef20685ac
buikiem <buivantrung1105@gmail.com>
2019-08-29 13:19:47 +0700

    122 - Remove Items At Checkout

--- src/shop/components/CheckoutItem/CheckoutItem.js
+++ src/shop/components/CheckoutItem/CheckoutItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 
-import { clearItemFromCart } from '../../redux/cart/cartActions';
+import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cartActions';
 
 import './CheckoutItem.scss';
 
@@ -9,7 +9,9 @@ import './CheckoutItem.scss';
 export const CheckoutItem = ({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
   const dispatch = useDispatch();
-  const clearItem = () => dispatch(clearItemFromCart(cartItem));
+  const onClearItem = () => dispatch(clearItemFromCart(cartItem));
+  const onAddItem = () => dispatch(addItem(cartItem));
+  const onRemoveItem = () => dispatch(removeItem(cartItem));
 
   return (
     <div className="checkout-item">
@@ -17,9 +19,13 @@ export const CheckoutItem = ({ cartItem }) => {
         <img src={imageUrl} alt="item"/>
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={onRemoveItem}>&#10094;</div>
+        {quantity}
+        <div className="arrow" onClick={onAddItem}>&#10095;</div>
+      </span>
       <span className="price">{price}</span>
-      <span className="remove-button" onClick={clearItem}>&#10008;</span>
+      <span className="remove-button" onClick={onClearItem}>&#10008;</span>
     </div>
   );
 };
