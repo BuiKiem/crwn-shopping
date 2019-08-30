3d1f3c520861a33927f4f922e2392a34baa0e588
buikiem <buivantrung1105@gmail.com>
2019-08-30 14:18:23 +0700

    130 - Collection Routing and Selector

--- src/shop/pages/CategoryPage/CollectionPage.js
+++ src/shop/pages/CategoryPage/CollectionPage.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import { CollectionItem } from '../../components/CollectionItem/CollectionItem';
+
+import { selectCollection } from '../../redux/collection/collectionSelector';
 
 import './CollectionPage.scss';
 
 
 export const CollectionPage = ({ match }) =>{
-  console.log(match.params.collectionId);
+  const collection = useSelector(selectCollection(match.params.collectionId));
+  console.log(collection);
 
   return (
     <div className="collection-page">
--- src/shop/redux/collection/collectionSelector.js
+++ src/shop/redux/collection/collectionSelector.js
@@ -1,9 +1,22 @@
 import { createSelector } from 'reselect';
 
 
-const selectCollection = (state) => state.collection;
+const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5
+};
+
+const selectCollectionState = (state) => state.collection;
 
 export const selectCollections = createSelector(
-  [selectCollection],
+  [selectCollectionState],
   (collection) => collection.collections
+);
+
+export const selectCollection = (collectionUrlParam) => createSelector(
+  [selectCollections],
+  (collections) => collections.find((collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
 );
