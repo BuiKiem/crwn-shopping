cf500b48501a4290a6cccd5c6420c0f234d95df8
buikiem <buivantrung1105@gmail.com>
2019-08-30 18:20:01 +0700

    134 - Data Flow in Our App

--- src/shop/components/CollectionOverview/CollectionsOverview.js
+++ src/shop/components/CollectionOverview/CollectionsOverview.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-import { selectCollections } from "../../redux/collection/collectionSelector";
+import { selectCollectionsForOverview } from "../../redux/collection/collectionSelector";
 
-import {CollectionPreview} from "../CollectionPreview/CollectionPreview";
+import { CollectionPreview } from "../CollectionPreview/CollectionPreview";
 
 import './CollectionsOverview.scss';
 
 
 export const CollectionsOverview = () => {
-  const collections = useSelector(selectCollections);
+  const collections = useSelector(selectCollectionsForOverview);
 
   return (
     <div className="shop-page">
